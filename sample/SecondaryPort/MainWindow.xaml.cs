2cd2af8cfccb418ac26a459895efcd7a8a82bc1a
Qu Long <qulong627@gmail.com>
2020-05-13 11:19:20 +0800

    Add UR Secondary sample code

---
+++ sample/SecondaryPort/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using URSecondaryLibrary;
+
+namespace SecondaryPort
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public IURSecondary urSec { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            urSec = IURSecondaryConnection.Create(IpAddress.Text);
+            var resp = urSec.Receive();
+            Logging.Text += resp + Environment.NewLine;
+        }
+
+        private void RunScript_Click(object sender, RoutedEventArgs e)
+        {
+            var cmds = URScript.Text.Split('\n');
+            for (int i=0; i<cmds.Length; i++)
+            {
+                urSec.Send(cmds[i]+'\n');
+            }
+
+            URScript.Text = "";
+
+            var resp = urSec.Receive();
+            Logging.Text += resp + Environment.NewLine;
+
+            LogScrollViewer.ScrollToBottom();
+        }
+    }
+}
