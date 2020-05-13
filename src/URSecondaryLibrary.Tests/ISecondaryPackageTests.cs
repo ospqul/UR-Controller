1e49cbe3d07db1ef94a7dd69a2027baa6ed92563
Qu Long <qulong627@gmail.com>
2020-05-13 12:00:54 +0800

    Add test cases for URSecondaryLibrary

---
+++ src/URSecondaryLibrary.Tests/ISecondaryPackageTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using Xunit;
+
+namespace URSecondaryLibrary.Tests
+{
+    public class ISecondaryPackageTests
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData("Test")]
+        [InlineData("Test\n")]
+        public void Pack_ValidateMethod(string command)
+        {
+            var expected = Encoding.ASCII.GetBytes(command);
+
+            var actual = ISecondaryPackage.Pack(command);
+
+            Assert.Equal(expected.Length, actual.Length);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.Equal(expected[i], actual[i]);
+            }
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("Test")]
+        [InlineData("Test\n")]
+        public void Unpack_ValidateMethod(string command)
+        {
+            var bytes = Encoding.ASCII.GetBytes(command);
+
+            var expected = Encoding.ASCII.GetString(bytes);
+
+            var actual = ISecondaryPackage.Unpack(bytes);
+
+            Assert.Equal(expected.Length, actual.Length);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ src/URSecondaryLibrary.Tests/IURSecondaryConnectionTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using Xunit;
+
+namespace URSecondaryLibrary.Tests
+{
+    public class IURSecondaryConnectionTests : IDisposable
+    {
+        public TcpListener server { get; set; }
+        public const string VALID_SERVER = "127.0.0.1";
+        public const int PORT = 30002;
+
+        public IURSecondaryConnectionTests()
+        {
+            IPAddress address = IPAddress.Parse(VALID_SERVER);
+
+            // Start a server
+            server = new TcpListener(address, PORT);
+            server.Start();
+        }
+
+        public void Dispose()
+        {
+            server.Stop();
+        }
+
+        [Fact]
+        public void Create_ValidIpPortShouldReturnConnected()
+        {
+            var urSecondary = IURSecondaryConnection.Create(VALID_SERVER);
+
+            Assert.NotNull(urSecondary);
+
+            var actual = urSecondary.IsConnected;
+
+            Assert.True(actual);
+        }
+
+        [Theory]
+        [InlineData("111.111.111.111", SocketError.TimedOut)]
+        public void Create_InvalidIpPortShouldReturnNotConnected(string server, SocketError error)
+        {
+            try
+            {
+                var urSecondary = IURSecondaryConnection.Create(server);
+
+                Assert.NotNull(urSecondary);
+
+                var actual = urSecondary.IsConnected;
+
+                Assert.False(actual);
+            }
+            catch (SocketException e)
+            {
+                Assert.Equal(error, e.SocketErrorCode);
+            }
+        }
+    }
+}
